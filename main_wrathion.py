472d34bd67eb5cb3a9808d0162dac1f5f5468d52
Coaixy <coaixy@outlook.com>
2023-07-28 17:29:02 +0800

    Feat:Add Login Version

---
+++ main_wrathion.py
@@ -0,0 +1,26 @@
+# @Author : Coaixy
+# @repo : https://www.github.com/coaixy/weiban-tool
+import Utils
+import time
+import json
+import pyperclip
+
+#通过Wrathion获取参数s
+data = json.loads(pyperclip.paste())
+tenantCode = data['tenantCode']
+userId = data['userId']
+x_token = data['token']
+userProjectId = data['userProjectId']
+#实例化对象
+main = Utils.main(tenantCode, userId, x_token, userProjectId)
+#初始化
+main.init()
+#获取列表
+finishIdList = main.getFinishIdList()
+print("加群讨论:https://jcdn.lawliet.ren/qrcode.jpg")
+print("开始运行")
+for i in main.getCourse():
+    main.start(i)
+    time.sleep(15)
+    main.finish(i,finishIdList[i])
+print("刷课完成")
