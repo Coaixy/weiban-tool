0ead29d46ed86480e305821fdfd659a7c7caba75
Coaixy <Coaixy@outlook.com>
2024-04-30 19:15:17 +0800

    Enhance : add select project id func

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ def get_project_id(user_id, tenant_code, token: str) -> str:
         print("已完成全部")
         exit(1)
     else:
-        return data[0]["userProjectId"]
+        # return data[0]["userProjectId"]
+        return data
 
 
 def get_tenant_code(school_name: str) -> str:
@@ -37,7 +38,7 @@ def get_tenant_code(school_name: str) -> str:
                 return j["code"]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     school_name = input("请输入学校名:")
     tenant_code = get_tenant_code(school_name=school_name)
     user_key = input("请输入账号:")
@@ -46,7 +47,9 @@ if __name__ == '__main__':
     print("验证码链接:")
     print(f"https://weiban.mycourse.cn/pharos/login/randLetterImage.do?time={now}")
     # 打开验证码
-    img_data = requests.get(f"https://weiban.mycourse.cn/pharos/login/randLetterImage.do?time={now}").content
+    img_data = requests.get(
+        f"https://weiban.mycourse.cn/pharos/login/randLetterImage.do?time={now}"
+    ).content
     with open("code.jpg", "wb") as file:
         file.write(img_data)
     file.close()
@@ -59,7 +62,7 @@ if __name__ == '__main__':
         "password": user_pwd,
         "tenantCode": tenant_code,
         "timestamp": now,
-        "verificationCode": verity_code
+        "verificationCode": verity_code,
     }
     ret = enco.login(payload)
     request_data = {"data": ret}
@@ -67,22 +70,31 @@ if __name__ == '__main__':
     text = requests.post(
         "https://weiban.mycourse.cn/pharos/login/login.do", data=request_data
     ).text
-    print(text)
+    print(text + "\n")
     json_data = json.loads(text)["data"]
 
+    # 获取所有课的实例
+    project_list = get_project_id(json_data["userId"], tenant_code, json_data["token"])
+    project_index = 0
+    for project_info in project_list:
+        print(str(project_index) + "." + project_info["projectName"])
+        project_index = project_index + 1
+    project_index = 0
+    project_index = int(input("请输入要刷的课程编号:"))
+    project_id = project_list[project_index]["userProjectId"]
     # 实例化对象
     main = Utils.main(
         tenant_code,
         json_data["userId"],
         json_data["token"],
-        get_project_id(json_data["userId"], tenant_code, json_data["token"]),
+        project_id,
     )
     print("加群讨论:https://jcdn.lawliet.ren/qrcode.jpg")
     print("开始运行")
     # 初始化
     main.init()
     # 获取列表
-    for chooseType in [2,3]:
+    for chooseType in [2, 3]:
         finishIdList = main.getFinishIdList(chooseType)
         num = len(finishIdList)
         index = 1
