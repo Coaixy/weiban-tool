1573fa0a8c5073318abc48110fce8ab8f419ab8d
Coaixy <Coaixy@outlook.com>
2024-08-10 13:39:00 +0800

    Feature:add cors in bank api

--- QuestionBank/QuestionBank.py
+++ QuestionBank/QuestionBank.py
@@ -4,8 +4,17 @@ import difflib
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],  # 允许的源
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许的方法，如：GET, POST, OPTIONS 等
+    allow_headers=["*"],  # 允许的头
+)
 
 
 def get_all_json_files_content(directory):
@@ -21,7 +30,7 @@ def get_all_json_files_content(directory):
     return json_files_content
 
 
-async def generate_bank():
+def generate_bank():
     final_result = {}
     directory = '.'  # 当前目录
     json_contents = get_all_json_files_content(directory)
@@ -45,19 +54,29 @@ async def generate_bank():
 
 
 bank_obj = {}
-with open("result.json", 'r') as f:
-    bank_obj = json.load(f)
 
 
 @app.get("/answer/{question}")
 async def get_answer(question: str):
+    for i in bank_obj:
+        if question in i:
+            return {'question': i, 'msg': bank_obj[i]}
     closest_match = difflib.get_close_matches(question, bank_obj.keys(), n=1, cutoff=0.6)
     if closest_match:
-        return {'question':closest_match[0],'msg': bank_obj[closest_match[0]]}
+        return {'question': closest_match[0], 'msg': bank_obj[closest_match[0]]}
     else:
         return {'msg': '题目不存在'}
     # return {'code': 200, 'msg': bank_obj.get(question, "题目不存在")}
 
 
-if __name__ == '__main__':
+def main():
+    generate_bank()
+    with open("result.json", 'r') as f:
+        global bank_obj
+        bank_obj = json.load(f)
+        print(bank_obj)
     uvicorn.run(app, host="127.0.0.1", port=8080)
+
+
+if __name__ == '__main__':
+    main()
