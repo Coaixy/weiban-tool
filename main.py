b9923251cc9f8419b3e0b5eabd59d17d2f87b925
imanong <isabyss@outlook.com>
2024-08-29 10:25:35 +0800

    Update some content

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import re
 import requests
 
 
-import QuestionBank.QuestionBank as Questio
+import QuestionBank.QuestionBank as QuestionBank
 import WeiBanHelper
 
 
@@ -57,6 +57,17 @@ _/    _/    _/  _/_/_/_/  _/  _/_/_/    _/    _/  _/    _/  _/_/_/_/_/  _/    _/
                 break
             else:
                 print("学校名称无效，仅允许中文字符，如果终端无法输入，请在外面输入，并复制粘贴到这里")
+    if 0 < len(arguments) < 6:
+        print_help_info()
+        exit(0)
+    if len(arguments) == 7:
+        account = arguments[0]
+        password = arguments[1]
+        school_name = arguments[2]
+        auto_verify = bool(arguments[3])
+        project_index = int(arguments[4])
+        auto_exam = int(arguments[5])
+        exam_threshold = int(arguments[6])
 
     Instance = WeiBanHelper.WeibanHelper(account=account, password=password, school_name=school_name,
                                          auto_verify=auto_verify,
@@ -75,17 +86,6 @@ _/    _/    _/  _/_/_/_/  _/  _/_/_/    _/    _/  _/    _/  _/_/_/_/_/  _/    _/
         auto_exam = int(input("是否自动考试: 0: 不自动考试, >0 : 考试时间(单位秒)"))
         if auto_exam >= 1:
             exam_threshold = int(input("允许错的题目数（如填0是一题不错，填1是可以错一题）: "))
-    if 0 < len(arguments) < 6:
-        print_help_info()
-        exit(0)
-    if len(arguments) == 7:
-        account = arguments[0]
-        password = arguments[1]
-        school_name = arguments[2]
-        auto_verify = bool(arguments[3])
-        project_index = int(arguments[4])
-        auto_exam = int(arguments[5])
-        exam_threshold = int(arguments[6])
 
     print("当前项目名称: ", Instance.project_list[project_index]['projectName'])
     Instance.run()
